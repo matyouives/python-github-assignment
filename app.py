7dca4924f75b09e05dcd93126967cb3b0927cd84
matyouives <mi163@usf.edu>
2025-11-23 20:44:35 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 print("Welcome to my Python program!")
+
+# Asking a user how much he slept
 hours = input("How many hours did you sleep today? ")
+
+# Ensuring it's a number
 try:
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number.")
     exit()
 
+# Converting hours to minutes
 minutes = hours * 60
 print(f"You've slept {minutes} minutes today.")
