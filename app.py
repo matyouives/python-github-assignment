7730f3248a5f0c9635028268febc1b2de98a2cb4
matyouives <mi163@usf.edu>
2025-11-23 20:40:57 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 print("Welcome to my Python program!")
 hours = input("How many hours did you sleep today? ")
-hours = float(hours)
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+
 minutes = hours * 60
 print(f"You've slept {minutes} minutes today.")
