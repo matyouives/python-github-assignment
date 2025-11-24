f6f364019e28aab548926422690d59725ecb13c4
matyouives <mi163@usf.edu>
2025-11-23 20:39:35 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 print("Welcome to my Python program!")
 hours = input("How many hours did you sleep today? ")
 hours = float(hours)
-minutes = hours / 60
+minutes = hours * 60
+print(f"You've slept {minutes} minutes today.")
